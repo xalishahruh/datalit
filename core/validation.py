94c74971bb802ffcc179c4c94a8a1c0cc383b717
NatalyaKim <00017636student@gmail.com>
2026-03-10 17:54:02 +0500

    M3: added simple validation rules

--- core/validation.py
+++ core/validation.py
@@ -1,3 +1,8 @@
-"""
-Validation functions for safe transformations.
-"""
+def range_check(df, column, min_val, max_val):
+    return df[(df[column] < min_val) | (df[column] > max_val)]
+
+def allowed_categories(df, column, allowed):
+    return df[~df[column].isin(allowed)]
+
+def non_null_violation(df, column):
+    return df[df[column].isna()]
